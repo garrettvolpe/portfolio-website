8804f10b905db8783f3139eb6e49a0edfbb7db74
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-23 20:08:55 -0500

    add navbar

---
+++ script.js
@@ -0,0 +1,12 @@
+// Select the body element
+const body = document.querySelector('body');
+
+// Add a scroll event listener to the window object
+window.addEventListener('scroll', function() {
+  // If the page has been scrolled, add the "scrolled" class to the body element
+  if (window.scrollY > 0) {
+    body.classList.add('scrolled');
+  } else {
+    body.classList.remove('scrolled');
+  }
+});
