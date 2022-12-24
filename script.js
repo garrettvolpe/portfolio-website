5067751d803ffa689c5c89a2705c6b41c9606847
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-23 20:10:41 -0500

    change y value for cat

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const body = document.querySelector('body');
 // Add a scroll event listener to the window object
 window.addEventListener('scroll', function() {
   // If the page has been scrolled, add the "scrolled" class to the body element
-  if (window.scrollY > 0) {
+  if (window.scrollY > 50) {
     body.classList.add('scrolled');
   } else {
     body.classList.remove('scrolled');
