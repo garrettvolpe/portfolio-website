3eb272089596b0b33bb658d1c556cad042986349
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-24 22:32:23 -0500

    cat eyeballs now functioning

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ window.addEventListener('scroll', function() {
     body.classList.remove('scrolled');
   }
 });
+
+
+
+function angle (cx,cy,ex,ey){
+  const dy = ey - cy
+  const dx = ex - cx;
+  const rad = Math.atan2(dy,dx) 
+  const deg = rad * 180 / Math.PI
+  return deg
+};
+
+document.addEventListener('mousemove', (e)=>{
+  const mouseX = e.clientX
+  const mouseY = e.clientY
+  const anchor = document.getElementById('anchor');
+  const rekt = anchor.getBoundingClientRect();
+  const anchorX = rekt.left + rekt.width /2;
+  const anchorY = rekt.top + rekt.height /2;
+  const atan2 = (anchorX, anchorY);
+  const angleDeg = angle(mouseX,mouseY,anchorX,anchorY)
+  console.log(angleDeg)
+  
+  const eyes = document.querySelectorAll('.eye');
+  eyes.forEach(eye => {
+      eye.style.transform = `rotate(${angleDeg + 90}deg)`
+  });
+});
