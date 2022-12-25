6d3792009c0c859c4d231cf8d4bb558a95ccbdb1
Garrett Volpe <109316247+garrettvolpe@users.noreply.github.com>
2022-12-25 09:39:50 -0500

    added comments

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// Select the body element
+const anchor = document.getElementById('anchor');
+const rekt = anchor.getBoundingClientRect();
+const anchorX = rekt.left + rekt.width /2;
+const anchorY = rekt.top + rekt.height /2;
+
 const body = document.querySelector('body');
 
-// Add a scroll event listener to the window object
+// fade cat out when scrolled.
 window.addEventListener('scroll', function() {
-  // If the page has been scrolled, add the "scrolled" class to the body element
   if (window.scrollY > 50) {
     body.classList.add('scrolled');
   } else {
     body.classList.remove('scrolled');
   }
 });
-
 
 
 function angle (cx,cy,ex,ey){
@@ -21,19 +23,14 @@ function angle (cx,cy,ex,ey){
   return deg
 };
 
+// Event listener for cats eyes to follow mouse.
 document.addEventListener('mousemove', (e)=>{
   const mouseX = e.clientX
   const mouseY = e.clientY
-  const anchor = document.getElementById('anchor');
-  const rekt = anchor.getBoundingClientRect();
-  const anchorX = rekt.left + rekt.width /2;
-  const anchorY = rekt.top + rekt.height /2;
   const atan2 = (anchorX, anchorY);
   const angleDeg = angle(mouseX,mouseY,anchorX,anchorY)
-  console.log(angleDeg)
-  
   const eyes = document.querySelectorAll('.eye');
   eyes.forEach(eye => {
-      eye.style.transform = `rotate(${angleDeg + 90}deg)`
+      eye.style.transform = `rotate(${angleDeg + 45}deg)`
   });
 });
